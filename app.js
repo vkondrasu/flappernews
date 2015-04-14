61b80f479c954755442a77ab0a8533be1ce8229b
vkondrasu <venkat.kondrasu@gmail.com>
2015-04-14 18:41:35 +0530

    Initial app

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ var app = angular.module('floppernews',[]);
 
 app.controller('maincontroller',['$scope', function($scope){
 	$scope.test = "Welcome to Angular JS!";
-	$scope.posts=['Mastering Algorithms','Getting and Cleaning data','Learn Foreign languages','A Game of thrones','Book store'];
+	$scope.posts=[{title:'Mastering Algorithms', timesVisited:1},
+					{title:'Getting and Cleaning data', timesVisited:1},
+					{title:'Learn Foreign languages', timesVisited:15},
+					{title: 'A Game of thrones',timesVisited:12},
+					{title: 'Book store', timesVisited:120}
+				];
 }]);
